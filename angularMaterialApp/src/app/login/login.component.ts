5e7cf0cd18f91a9a6763b0c9ee8c8101e3dca71e
shraddha <shrashahane@gmail.com>
2024-11-11 23:01:47 +0530

    initial Commit

---
+++ angularMaterialApp/src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  standalone: false
+})
+export class LoginComponent implements OnInit {
+  // loginForm!: FormGroup;  
+
+  // constructor(private fb: FormBuilder){
+
+  // }
+  ngOnInit(){
+    
+  }
+
+}
